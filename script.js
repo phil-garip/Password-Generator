4e94e5ace93d1523039a7eb14597d17a312462f2
Philip Garip <phil.garip@gmail.com>
2021-04-15 19:17:46 -0400

    added functions to get User Input

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //Starting Data===========================================
 var generateBtn = document.querySelector("#generate");
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var abcUpperArr = abcUpper.split("");
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var abcLowerArr = abcLower.split("");
-var numbers = ["0123456789"];
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcUpperArr = upperCase.split("")
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var abcLowerArr = lowerCase.split("");
+var numbers = "0123456789";
 var numArr = numbers.split("");
-var spCharacters = ["!@#$%^&*().,/?`~-+=[]{}"];
+var spCharacters = "!@#$%^&*().,/?`~-+=[]{}";
 var spCharacterArr = spCharacters.split("");
+var userArray = [];
+var pwArray = [];
 
 
 // Dependencies ===========================================
@@ -15,7 +17,93 @@ var spCharacterArr = spCharacters.split("");
 
 // Functions ===============================================
   // generate password
-  function generatePassword() {
+  function generatePassword() {}
+  
+  function getUserLength() {
+    var userLength = prompt("how many characters? Please enter a number between 8 and 128");
+    if (userLength > 7 && userLength < 129) {
+    } else {
+      alert("Invalid number. Please try again.")
+      getUserLength();
+    }
+  }
+
+  function getUserUpper() {
+    var userUpper = confirm("Would you like uppercase letters?");
+    if (userUpper) {
+      userArray.push(userArray.concat(abcUpperArr));
+    } 
+  }
+
+  function getUserLower() {
+    var userLower = confirm("would you like lowercase letters?");
+    if (userLower) {
+      userArray.push(userArray.concat(abcLowerArr));
+    } 
+  }
+
+  function getUserNums() {
+    var userNums = confirm("Would you like numbers?");
+    if (userNums) {
+      userArray.push(userArray.concat(numArr));
+    } 
+  }
+
+  function getUserSpecials() { 
+    var userSpecial = confirm("Would you like special characters?");
+    if (userSpecial) {
+      userArray.push(userArray.concat(spCharacterArr));
+    }
+  }
+
+
+function getUserChoices(){
+  getUserLength();
+  getUserUpper();
+  getUserLower();
+  getUserNums();
+  getUserSpecials();
+  
+}
+  getUserChoices();
+  console.log(pwArray.concat(userArray));
+    
+    
+    // function getUserChoices() {
+      //   var userLength = prompt("how many characters? Please enter a number between 8 and 128");
+      //   var userUpper = confirm("Would you like uppercase letters?");
+      //   var userLower = confirm("would you like lowercase letters?");
+      //   var userNums = confirm("Would you like numbers?");
+      //   var userSpecial = confirm("Would you like special characters?");
+      // }
+
+    // if (!(7 < userLength < 129)) {
+    //   alert("This is not a valid number, please enter a number greater than 7 and less than 129.");
+    //   return getUserChoices();
+    // }
+    // else if (userUpper) {
+    //   userArray.push(userArray.concat(abcUpperArr));
+    // }
+    // else if (userLower) {
+    //   userArray.push(userArray.concat(abcLowerArr));
+    // }    
+    // else if (userNums) {
+    //   userArray.push(userArray.concat(numArr));
+    // } 
+    // else if (userSpecial) {
+    //   userArray.push(userArray.concat(spCharacterArr));      
+    // }
+    //     }
+      
+  // getUserChoices();
+  // console.log(userArray);
+
+
+//userArray.push(abcUpperArr.concat(abcLowerArr, numArr, spCharacterArr));
+
+
+
+
     // get user choices (it's own function)
       // how long?
         //minimum of 8 characters maximum of 128 characters
@@ -31,7 +119,7 @@ var spCharacterArr = spCharacters.split("");
   
     // return a password
   
-  }
+  
 
 
 // User Interractions ======================================
