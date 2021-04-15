1f3f125a156917ad00b7e37a3bdb358074f8d3c9
Philip Garip <phil.garip@gmail.com>
2021-04-15 14:54:39 -0400

    added pseudo code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Starting Data
+//Starting Data===========================================
 var generateBtn = document.querySelector("#generate");
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var abcUpperArr = abcUpper.split("");
@@ -10,10 +10,67 @@ var spCharacters = ["!@#$%^&*().,/?`~-+=[]{}"];
 var spCharacterArr = spCharacters.split("");
 
 
+// Dependencies ===========================================
+  // generate button
+
+// Functions ===============================================
+  // generate password
+  function generatePassword() {
+    // get user choices (it's own function)
+      // how long?
+        //minimum of 8 characters maximum of 128 characters
+          //say try again
+        //store their choice
+      // do you want uppercase letters? store their choice
+      // do you want numbers? store their choice
+      // do you want special characters? store their choice
+
+    // combine chosen arrays into larger array (its own function)
+
+    // randomly choose characters from the combined array and add them to a string called password
+  
+    // return a password
+  
+  }
+
+
+// User Interractions ======================================
+  // user clicks to generate password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Functions
+var randomList = [];
 function generatePassword() {
-  return password;
+  //confirm spCharacters
+  var confirmSpecial = confirm("Would you like special characters?")
+  //add random special caharacter to list
+  if (confirmSpecial === true) {
+    randomList.charAt(Math.floor(Math.random() * randomList.length));
+  }
 }
 
 
