273c028d26cd1e2dfb9d2375a680f31dc09436fc
Philip Garip <phil.garip@gmail.com>
2021-04-15 22:31:34 -0400

    added for loop and return statement to generatePassword function

--- script.js
+++ script.js
@@ -11,12 +11,6 @@ var spCharacterArr = spCharacters.split("");
 var userArray = [];
 var pwArray = [];
 var pwLength = 0;
-
-// Dependencies ===========================================
-  // generate button
-
-// Functions ===============================================
-  // generate password
   
   function getUserLength() {
     var userLength = prompt("how many characters? Please enter a number between 8 and 128");
@@ -66,77 +60,6 @@ function getUserChoices() {
   getUserSpecials();
 }
   getUserChoices();
-  
- 
-  var password = userArray[Math.floor(Math.random() * userArray.length)];
-  
-  for (var i = 0; i < pwLength; i++) {
-    pwArray.push(i);
-  }
-
-  console.log(pwLength, pwArray, password);
-
-
-  
-
-
-    
-    
-    // function getUserChoices() {
-      //   var userLength = prompt("how many characters? Please enter a number between 8 and 128");
-      //   var userUpper = confirm("Would you like uppercase letters?");
-      //   var userLower = confirm("would you like lowercase letters?");
-      //   var userNums = confirm("Would you like numbers?");
-      //   var userSpecial = confirm("Would you like special characters?");
-      // }
-
-    // if (!(7 < userLength < 129)) {
-    //   alert("This is not a valid number, please enter a number greater than 7 and less than 129.");
-    //   return getUserChoices();
-    // }
-    // else if (userUpper) {
-    //   userArray.push(userArray.concat(abcUpperArr));
-    // }
-    // else if (userLower) {
-    //   userArray.push(userArray.concat(abcLowerArr));
-    // }    
-    // else if (userNums) {
-    //   userArray.push(userArray.concat(numArr));
-    // } 
-    // else if (userSpecial) {
-    //   userArray.push(userArray.concat(spCharacterArr));      
-    // }
-    //     }
-      
-  // getUserChoices();
-  // console.log(userArray);
-
-
-//userArray.push(abcUpperArr.concat(abcLowerArr, numArr, spCharacterArr));
-
-
-
-
-    // get user choices (it's own function)
-      // how long?
-        //minimum of 8 characters maximum of 128 characters
-          //say try again
-        //store their choice
-      // do you want uppercase letters? store their choice
-      // do you want numbers? store their choice
-      // do you want special characters? store their choice
-
-    // combine chosen arrays into larger array (its own function)
-
-    // randomly choose characters from the combined array and add them to a string called password
-  
-    // return a password
-  
-  
-
-
-// User Interractions ======================================
-  // user clicks to generate password
 
 
 // Dependencies (HTML Elements, Libraries, etc.) ==============
@@ -147,7 +70,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Functions =========================================================
 function generatePassword() {
-  return "a string"
+  for (var i = 0; i < pwLength; i++) {
+    pwArray.push(userArray[Math.floor(Math.random() * userArray.length)]);
+  }
+  return pwArray.join("");
 }
 
 
